0cb4aa3b8c6d4361665700961bc9efb30b749379
Stefan Schindler <s.schindler.de@gmail.com>
2020-08-26 14:34:05 +0200

    change database

--- src/backend/database/databaseConnector.ts
+++ src/backend/database/databaseConnector.ts
@@ -7,6 +7,6 @@ import neo4j from "neo4j-driver";
  */
 
 export const db = neo4j.driver(
-    "bolt://jlu-buster.mni.thm.de:10211",
+    "bolt://jlu-buster.mni.thm.de:12321",
     neo4j.auth.basic("neo4j", "1234")
 );
